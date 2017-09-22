142ee24883079efbb8aae935cd67385fa0977b03
Timur <timur.salakhetdinov@gmail.com>
2017-09-22 10:01:50 +0300

    clean code

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -2,17 +2,41 @@ public class FizzBuzz {
     public static String answer(int n) {
         if (isValid(n))
             throw new NumberOutOfRangeException();
-        if (n % 3 == 0 && n % 5 == 0)
-            return "FizzBuzz";
-        if (n % 3 == 0)
-            return "Fizz";
-        if (n % 5 == 0)
-            return "Buzz";
-        return String.valueOf(n);
+        return fizzBuzz(n);
     }
 
     private static boolean isValid(int n) {
         return n < 1 || n > 100;
+    }
+
+    private static String fizzBuzz(int n) {
+        if (isFizzBuzz(n))
+            return "FizzBuzz";
+        return fizz(n);
+    }
+
+    private static boolean isFizzBuzz(int n) {
+        return isFizz(n) && isBuzz(n);
+    }
+
+    private static String fizz(int n) {
+        if (isFizz(n))
+            return "Fizz";
+        return buzz(n);
+    }
+
+    private static boolean isFizz(int n) {
+        return n % 3 == 0;
+    }
+
+    private static String buzz(int n) {
+        if (isBuzz(n))
+            return "Buzz";
+        return String.valueOf(n);
+    }
+
+    private static boolean isBuzz(int n) {
+        return n % 5 == 0;
     }
 
     public static class NumberOutOfRangeException extends RuntimeException {
